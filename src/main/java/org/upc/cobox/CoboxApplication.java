975ea2d15dcc8d7c926808fb32b76c0bf2a9e04c
david alexander <davidalexd1234@gmail.com>
2025-10-08 15:07:43 -0500

    feat(init):template for backend cobox(included context iam and shared)

--- src/main/java/org/upc/cobox/CoboxApplication.java
+++ src/main/java/org/upc/cobox/CoboxApplication.java
@@ -2,8 +2,10 @@ package org.upc.cobox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CoboxApplication {
 
     public static void main(String[] args) {
